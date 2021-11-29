755b7bed53f915caca00dac1282f023bf04aa36c
sasam2 <33865438+sasam2@users.noreply.github.com>
2021-11-29 23:44:38 +0000

    Deleted function "buildExpression()" and replaced it with "buildExpressionWithParenthesis()", which handles expressions with parenthesis and throws exceptions on wrong input.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 #include <stdio.h>  
 #include <algorithm>
+#include <string>
 using namespace std;
 
 
@@ -69,72 +70,102 @@ class op: public operation {
   }
 };
 
-operation *buildExpression(string expression){
-    
-    if(expression.size()==1 && '0' <= expression[0] && expression[0] <= '9')
-        return new nr(expression[0]-'0');
-    
-        
-    op *prevOp=NULL;
-    op *currOp=NULL;
-    op *firstOp=NULL;
-    for(int i=0; i<expression.size(); ){
-        
-        int a = expression[i++]-'0';
-        if(a < 0  || a > 9){
-            cerr<<"Error: found non digit "<< a <<endl;
-            if(firstOp!=NULL)
-                delete firstOp;
-            return NULL;
-        }
 
-        if(i==expression.size()){ //expression ended
-            prevOp->right = new nr(a);
-            //cout << a << ')' << endl;
-            break;
-        }
-        
-        char sign = expression[i++];
-        if(sign != '-' && sign != '+' && sign != '/' && sign != '*'){
-            cerr << "Error: unknown sign " << sign <<endl;
-            if(firstOp!=NULL)
-                delete firstOp;
-            return NULL; 
-        }
+int findClosingParenthesisIndex(string expression){
 
-        if(prevOp!=NULL && (prevOp->sign == '*' || prevOp->sign == '/') && (sign == '-' || sign == '+')){
-                        
-                //close expression of * or /          
-                prevOp->right = new nr(a);
-    
-                //create new parent node "firstOp" that has current tree on left node
-                currOp = new op();
-                currOp->left = firstOp;
-                currOp->sign = sign;
-                
-                prevOp=currOp;
-                firstOp=currOp;
+	int parenthesisCount=0;
+	for(int i=1; i<expression.size(); i++){
+		if(parenthesisCount==0 && expression[i]==')')
+		{ 
+			return i;
 
-            //cout << a << sign << ' ';
-        } else { //(sign == '-' || sign == '+' || sign == '/' || sign == '*')
-    
-            //by default rightmost operands are the closest to the leaves of the tree
-            currOp = new op();
-            currOp->left = new nr(a);
-            currOp->sign = sign;
-            //cout << "(" << a << sign;
-            if(prevOp!=NULL){
-                 prevOp->right=currOp;
-            }
-        }
-        //first iteration
-        if(prevOp==NULL){
-              firstOp=currOp;        
-        }        
-        prevOp=currOp;
+		}
+		if(expression[i]=='(') parenthesisCount++;
+		if(expression[i]==')') parenthesisCount--;
+		
+	}
+	return -1;
 
-    }
-    return firstOp;
+}
+
+operation *buildExpressionWithParenthesis(string expression){
+	
+	//cout<<"expression: "<<expression<<endl;
+	if(expression.size()==0) return NULL;	
+
+	
+	if(expression.size()==1){
+		if('0' > expression[0] || expression[0] > '9')	throw invalid_argument("Expression should be a 0-9 digit!");
+    	return new nr(expression[0]-'0');
+	}
+		
+	char firstChar=expression[0];
+	int i=1;
+	operation *leftOp;
+	if(firstChar=='(')
+	{
+		i = findClosingParenthesisIndex(expression);
+		if(i<0) throw invalid_argument("Wrong parenthesis schema!");
+		leftOp=buildExpressionWithParenthesis(expression.substr(1, i-1));
+		if(i==expression.size())
+			return leftOp;
+		
+		i++;
+	} else {
+		leftOp=buildExpressionWithParenthesis(expression.substr(0, 1));
+	}
+		
+
+	//cout<<endl;
+	char sign=expression[i];
+	if(sign != '-' && sign != '+' && sign != '/' && sign != '*')
+         throw invalid_argument("Error: unknown sign!");
+	
+	//cout<<"sign "<<sign<<endl;
+	operation *rightOp;
+	for(; sign == '*' || sign == '/';){
+		
+		//cout<<"iOp "<<i <<endl;		
+		//cout<<"signOp "<<expression[i] <<endl;
+		int firstIdx = i+1;
+		char rightChar=expression[firstIdx];
+		if(rightChar=='('){
+			int parenthesisExpressionLenght=findClosingParenthesisIndex(expression.substr(firstIdx, expression.size()));
+			if(i<0) 
+				throw invalid_argument("Wrong parenthesis schema!");
+			i = firstIdx+parenthesisExpressionLenght;
+			//cout<<"i pranthesis="<<i<<endl;
+			//cout<<"expr "<<expression<<endl;
+			rightOp=buildExpressionWithParenthesis(expression.substr(firstIdx+1, parenthesisExpressionLenght-1));
+			//cout<<"original "<<expression.substr(firstIdx, expression.size())<<endl;
+			//cout<<"paranthesis "<<expression.substr(firstIdx+1, parenthesisExpressionLenght)<<endl;
+			i++;
+			//cout<<"i="<<i<<endl;
+		} else {
+			int right=rightChar-'0';
+			rightOp=buildExpressionWithParenthesis(expression.substr(firstIdx, 1));
+			i+=2;
+			//cout<<"number "<<right<<endl;
+		}
+		leftOp=new op(leftOp, rightOp, sign);
+	
+		if(i>=expression.size())
+			return leftOp;
+		sign=expression[i];
+
+		if(sign != '-' && sign != '+' && sign != '/' && sign != '*')
+            throw invalid_argument("Error: unknown sign!");
+		
+	}
+	//cout<<"end i="<<i<<endl;
+	//cout<<"rightOp "<< expression.substr(i+1, expression.size()-1)<<endl;
+	rightOp=buildExpressionWithParenthesis(expression.substr(i+1, expression.size()-1));
+	//cout <<endl;
+
+
+	return new op(leftOp, rightOp, sign);
+	
+
 }
 
 string removeSpaces(string str){
@@ -152,22 +183,21 @@ void test(string str){
     cout<<"original expression: "<<str<<endl;    
     string strNoSpc=removeSpaces(str);
 
-    cout<<"expression without spaces: "<<strNoSpc<<endl;
-    operation *firstOp = buildExpression(strNoSpc);
-    if(firstOp==NULL) {
-        cout << endl;
-        return;
-    }
+    try {
+        cout<<"expression without spaces: "<<strNoSpc<<endl;
+    	operation *firstOp = buildExpressionWithParenthesis(strNoSpc);
 
-    cout<<"expression with precedence: ";    
-    firstOp->print(cout);
-    cout << endl;
+		cout<<"expression with precedence: ";    
+		firstOp->print(cout);
+		cout << endl;
 
-    cout<<"expression result: " << firstOp->exec() <<endl;
-    cout<<endl;
+		cout<<"expression result: " << firstOp->exec() <<endl;
+		cout<<endl;
 
-    //cout<<"delete"<<endl;
-    delete firstOp;
+		delete firstOp;
+    } catch (std::invalid_argument const& ex) {
+        std::cout << "Invalid argument! " << ex.what() << endl << endl;
+    }    
 }
 
 int main(int argc, char* argv[]) {
@@ -179,12 +209,17 @@ int main(int argc, char* argv[]) {
     cout<<endl;
 
     test("4");
+	test("4 * 3 / 2 + 6+1*5 * 2");
     test("4 * 3 / 2 + 6+1*5 * 2");
+    test("4 * 3 / 2 *6 + 6+1*5 * 2");
     test("4 * 3 / 2 + 5 * 2");
-    test("4 + 3 / 2 + 5 * 2 *9");
-    test("4 . 3 / 2 + 5 * 2");
-    test("a . 3 / 2 + 5 * 2");
-
+    test("((4 + 1) - 3) / 2 + 5 * 2 *9");
+    test("4 * (3 + 2) + 5 * 2");
+    test("1 * 3 / 2 + 5 * 2");
+	test("1 * (3 / (1+2))");
+	test(" ((3) / 2 + 5 * 2");
+	test("1 a (3 / (1+2))");
+	test("a (3 / (1+2))");
    
    return 0;
 }
